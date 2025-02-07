b08f82e5ae2d39c9019d17ea84482c568797657b
Rohit Gupta <rohitguptar964@gmail.com>
2025-02-07 23:19:41 +0530

    Updated port to use environment variable

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const http = require("http");
-
+const port = process.env.PORT || 3001;
 http.createServer((req, res) => {
   res.write("Hello, I am on the server now");
   res.end();
-}).listen(3001, () => {
+}).listen(port, () => {
   console.log("Server is connected");
 });
